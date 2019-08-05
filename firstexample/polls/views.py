bc4e53b76c07bd39aa4290dc5353d91938ea58d6
Luan C. Redmann <luandacostaredmann@gmail.com>
2019-08-05 09:00:19 -0300

    It's adding a http response to the first view at the index request.

--- firstexample/polls/views.py
+++ firstexample/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    response = "Olá, mundo! Bem-vindos à primeira aplicação."
+    return HttpResponse(response)
