859b052c6b190a3c3f5638ad5546d3f84363d671
Luan C. Redmann <luandacostaredmann@gmail.com>
2019-08-12 20:13:47 -0300

    It's adding the Question model.

--- firstexample/polls/models.py
+++ firstexample/polls/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_lenght = 200)
+    pub_date = models.DateTimeField("Publication date")
